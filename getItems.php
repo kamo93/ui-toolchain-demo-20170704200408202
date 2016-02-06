aaa5c6964780d477e41f5de10cfcba5d0ce6a02a
hmagph <hmagph@users.noreply.github.com>
2016-02-06 23:43:45 +0100

    Update getItems.php

--- getItems.php
+++ getItems.php
@@ -8,7 +8,11 @@ function RetrieveItems()
     $application_json = json_decode($application, true);
     $applicationURI = $application_json["application_uris"][0];
     //echo "\r\napplicationURI:" . $applicationURI;
-    $catalogHost = str_replace("-ui-", "-catalog-api-", $applicationURI);
+	if (substr( $applicationURI, 0, 3 ) === "ui-") {
+		$catalogHost = "catalog-api-" . substr($applicationURI, 3);
+	} else {
+		$catalogHost = str_replace("-ui-", "-catalog-api-", $applicationURI);
+	}
     //echo "\r\ncatalogHost:" . $catalogHost;    
     $catalogRoute = "http://" . $catalogHost;
     //echo "\r\ncatalogRoute:" . $catalogRoute;    
