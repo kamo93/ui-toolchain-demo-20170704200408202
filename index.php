3bf03c32e50dd744e57681195cec0e35fa563bfb
Robin Bobbitt <ryehle@us.ibm.com>
2016-02-09 23:31:19 -0500

    Task 54140: automatically start load from pipeline rather than requiring start button in UI - update link from main page

--- index.php
+++ index.php
@@ -100,7 +100,7 @@ function orderItem(itemID){
 		<div id='boxes' class="notes"></div>
 	</div>
 	<div id="loading"><br>Loading...</div>
-	<a href="./catalogLoadTest.html">Catalog Load Tester</a>
+	<a href="./autoLoadTest.html">Catalog Load Tester</a>
 </body>
 </html>
 
